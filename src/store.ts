c3202a69e850a1e929273cbf5c6e0c42ce54be2b
Hitheshnani <125653156+Hitheshnani@users.noreply.github.com>
2025-08-09 10:17:41 +0530

    Update store.ts

--- src/store.ts
+++ src/store.ts
@@ -1,13 +1,14 @@
-import { configureStore } from '@reduxjs/toolkit'
-import formBuilderReducer from './slices/formBuilderSlice'
-import savedFormsReducer from './slices/savedFormsSlice'
+import { configureStore } from '@reduxjs/toolkit';
+import formBuilderReducer from './slices/formBuilderSlice';
+import savedFormsReducer from './slices/savedFormsSlice';
 
 export const store = configureStore({
   reducer: {
     formBuilder: formBuilderReducer,
     savedForms: savedFormsReducer,
   },
-})
+});
 
-export type RootState = ReturnType<typeof store.getState>
-export type AppDispatch = typeof store.dispatch
+// Infer the `RootState` and `AppDispatch` types from the store itself
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
